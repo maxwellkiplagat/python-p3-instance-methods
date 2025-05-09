4fd31a837a759b9f5f739e30d491ed3cb8b910be
maxwellkiplagat <maxwellkiplagat0@gmail.com>
2025-05-09 14:06:44 +0300

    finished  python-p3-instamce-methods

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def sit(self):
+        print("The dog is sitting.")
+    def bark(self):
+        print("Woof!")
     # Class body goes here
 
     #Instance method definition
-    pass
+    
